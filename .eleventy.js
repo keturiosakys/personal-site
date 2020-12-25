e45e97c8063cbac003db0deaea0d0d921f29675e
Laurynas Keturakis <keturakis@hey.com>
2020-12-25 02:21:54 +0200

    initial commit + navbar rudiments

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,25 @@
 const htmlmin = require("html-minifier");
+const yaml = require("js-yaml");
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
 
-  eleventyConfig.addWatchTarget("./_tmp/style.css");
-
-  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+  eleventyConfig.addWatchTarget("./_tmp/static/css/style.css");
 
   eleventyConfig.addPassthroughCopy({
-    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+    "./_tmp/style.css": "./style.css",
+    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js"
   });
+
+  //Syntax highlighting for code blocks
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  //Support for YAML in _data
+  eleventyConfig.addDataExtension("yaml", (contents) =>
+  yaml.safeLoad(contents)
+  );
 
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
@@ -31,4 +41,12 @@ module.exports = function (eleventyConfig) {
 
     return content;
   });
+  // Let Eleventy transform HTML files as nunjucks
+  // So that we can use .html instead of .njk
+  return {
+    dir: {
+      input: "src",
+    },
+    htmlTemplateEngine: "njk",
+  };
 };
