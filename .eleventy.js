4bf5af83ad2391365495707e5882fe993a281128
Laurynas Keturakis <keturakis@hey.com>
2020-12-26 03:00:58 +0200

    reinit + nav + hero text

---
+++ .eleventy.js
@@ -0,0 +1,54 @@
+const htmlmin = require("html-minifier");
+const yaml = require("js-yaml");
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+
+  eleventyConfig.addWatchTarget("./_tmp/static/css/style.css");
+
+  eleventyConfig.addPassthroughCopy({
+    "./_tmp/style.css": "./style.css",
+    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+  });
+
+  eleventyConfig.addLayoutAlias("default", "default.html");
+
+  //Syntax highlighting for code blocks
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  //Support for YAML in _data
+  eleventyConfig.addDataExtension("yaml", (contents) =>
+    yaml.safeLoad(contents)
+  );
+
+  eleventyConfig.addShortcode("version", function () {
+    return String(Date.now());
+  });
+
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith(".html")
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+  // Let Eleventy transform HTML files as nunjucks
+  // So that we can use .html instead of .njk
+  return {
+    dir: {
+      input: "src",
+    },
+    htmlTemplateEngine: "njk",
+  };
+};
