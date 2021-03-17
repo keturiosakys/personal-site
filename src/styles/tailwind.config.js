e0de1e88e168c7f19105ca0022f138152a140da8
Laurynas Keturakis <keturakis@hey.com>
2021-03-17 18:06:33 +0100

    some content updates and removed dark mode

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  darkMode: "media",
+  darkMode: "false",
   purge: {
     content: ["_site/**/*.html"],
     options: {
@@ -10,21 +10,14 @@ module.exports = {
     extend: {
       colors: {
         theme: "var(--background-theme)",
-        themeDark: "var(--dark-background-theme)",
         primary: "var(--color-primary)",
         secondary: "var(--color-secondary)",
         tertiary: "var(--color-tertiary)",
-        primaryDark: "var(--dark-color-primary)",
-        secondaryDark: "var(--dark-color-secondary)",
-        tertiaryDark: "var(--dark-color-tertiary)",
 
         contentColor: "var(--color-content)",
         colorAccent: "var(--color-accent)",
 
         colorMarker: "var(--color-marker)",
-        contentColorDark: "var(--dark-color-content)",
-        colorAccentDark: "var(--dark-color-accent)",
-        colorMarkerDark: "var(--dark-color-marker)",
       },
     },
   },
